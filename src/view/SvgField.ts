bb567d544541d518469f99e87a9ea423289ec489
nielinjie <nielinjie@gmail.com>
2020-05-08 10:40:29 +0800

    dev raw implementation

--- src/view/SvgField.ts
+++ src/view/SvgField.ts
@@ -2,7 +2,7 @@ import React from "react";
 import InlineSVG from "svg-inline-react";
 
  const SvgField = (props: any) => {
-  //TODO 不支持jsx/tsx， 因为后台编译直接采用tsc，没有经过jsx编译。
+  //NOTE 不支持jsx/tsx， 因为后台编译直接采用tsc，没有经过jsx编译。
   return React.createElement(InlineSVG, { src: props?.record?.svg  });
 };
 
